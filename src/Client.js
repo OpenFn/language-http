c73cfc9a752b792e9c65d764966bf191be9a0a97
Taylor Downs <downs.taylor@gmail.com>
2019-12-06 15:29:48 +0000

    include formData in client

--- src/Client.js
+++ src/Client.js
@@ -1,53 +1,59 @@
-import request from 'request'
+import request from 'request';
 import { assembleError, tryJson } from './Utils';
 
 export function rawRequest(params) {
   return new Promise((resolve, reject) => {
     request(params, (error, response, body) => {
-      error = assembleError({error, response})
+      error = assembleError({ error, response });
       error && reject(error);
 
       console.log(`✓ Request succeeded.`);
-      console.log(`Server responded with: \n${JSON.stringify(response, null, 2)}`);
+      console.log(
+        `Server responded with: \n${JSON.stringify(response, null, 2)}`
+      );
       const resp = tryJson(body);
       resolve(resp);
-    })
-  })
+    });
+  });
 }
 
-export function req( method, params ) {
+export function req(method, params) {
   const { url, headers, body, auth, query, rest } = params;
   return new Promise((resolve, reject) => {
     const j = request.jar();
-    request ({
-      url,
-      headers,
-      auth,
-      qs: query,
-      method: method,
-      json: body,
-      jar: j,
-      ...rest
-    }, function(error, response, body){
-      error = assembleError({error, response})
-      if(error) {
-        reject(error);
-      } else {
-        console.log(`✓ ${method} succeeded.`);
-        console.log(`Server responded with: \n${JSON.stringify(response, null, 2)}`);
-        const resp = tryJson(body);
-        if(rest.keepCookie) {
-          const __cookie = j.getCookieString(url);
-          resolve({
-            __cookie,
-            ...resp
-          });
+    request(
+      {
+        url,
+        headers,
+        auth,
+        qs: query,
+        method: method,
+        json: body,
+        formData: formData,
+        jar: j,
+        ...rest,
+      },
+      function(error, response, body) {
+        error = assembleError({ error, response });
+        if (error) {
+          reject(error);
         } else {
-          resolve(
-            resp
+          console.log(`✓ ${method} succeeded.`);
+          console.log(
+            `Server responded with: \n${JSON.stringify(response, null, 2)}`
           );
+          const resp = tryJson(body);
+          if (rest.keepCookie) {
+            const __cookie = j.getCookieString(url);
+            resolve({
+              __cookie,
+              ...resp,
+            });
+          } else {
+            resolve(resp);
+          }
         }
       }
-    })
-  })
+    );
+  });
 }
